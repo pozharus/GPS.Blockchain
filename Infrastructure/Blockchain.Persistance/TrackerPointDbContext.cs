55ae8d5951858eeaab8b3b9a6c0819ad566d2050
pozharus <y.v.pozharskyi@gmail.com>
2022-12-13 12:45:51 +0100

    Small fixes

--- Infrastructure/Blockchain.Persistance/TrackerPointDbContext.cs
+++ Infrastructure/Blockchain.Persistance/TrackerPointDbContext.cs
@@ -3,6 +3,7 @@ using Blockchain.Application.Points.Queries.GetPointDetails;
 using Blockchain.Application.Points.Queries.GetPointList;
 using Blockchain.Domain;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Blockchain.Persistance
 {
@@ -27,26 +28,20 @@ namespace Blockchain.Persistance
         }
         public PointDetailsVm getPointById(GetPointDetailsQuery request)
         {
-            var assets = BigChainDbAPI.getAssets(request.Id.ToString());
-            PointDetailsVm point = new PointDetailsVm();
-            foreach (var item in assets)
+            var asset = BigChainDbAPI.getAssets(request.Id.ToString());
+            return new PointDetailsVm
             {
-                if(item.Data.id == request.Id)
-                {
-                    point.transactionId = item.Id;
-                    point.timestamp = item.Data.timestamp;
-                    point.latitude = item.Data.latitude;
-                    point.longitude = item.Data.longitude;
-                    point.altitude = item.Data.altitude;
-                    point.speed = item.Data.speed;
-                    point.satelites = item.Data.satelites;
-                    point.delusionOfPresition = item.Data.delusionOfPresition;
-                    point.horizontalDelusionOfPresition = item.Data.horizontalDelusionOfPresition;
-                    point.verticalDelusionOfPresition = item.Data.verticalDelusionOfPresition;
-                }
-                break;
-            }
-            return point;
+                transactionId = asset.First().Id,
+                timestamp = asset.First().Data.timestamp,
+                latitude = asset.First().Data.latitude,
+                longitude = asset.First().Data.longitude,
+                altitude = asset.First().Data.altitude,
+                speed = asset.First().Data.speed,
+                satelites = asset.First().Data.satelites,
+                delusionOfPresition = asset.First().Data.delusionOfPresition,
+                horizontalDelusionOfPresition = asset.First().Data.horizontalDelusionOfPresition,
+                verticalDelusionOfPresition = asset.First().Data.verticalDelusionOfPresition
+            };
         }
         public string createPoint(TrackerPoint point)
         {
